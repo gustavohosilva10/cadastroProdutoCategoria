2e5a4953cc628d4ffd23c4fa883acbee9bd8f804
Gustavo henrique oliveira da silva <47875693+gustavohosilva10@users.noreply.github.com>
2020-11-28 20:55:59 -0300

    up

--- resources/views/novoproduto.blade.php
+++ resources/views/novoproduto.blade.php
@@ -11,16 +11,13 @@
                 <label for="nome">Nome</label>
                 <input type="text" class="form-control" name="nome" 
                        id="nome" placeholder="produto">
-                       <label for="preco">Preço</label>
-                       <input type="text" class="form-control" name="nome" 
-                              id="preco" placeholder="preco">
-
-                              <div class="form-group">
-             
-                            </div>
             </div>
           
-       
+            <div class="form-group">
+                <label for="preco">Preço</label>
+                <input type="text" class="form-control" name="nome" 
+                       id="preco" placeholder="preco">
+            </div>
             <button type="submit" class="btn btn-primary btn-sm">Salvar</button>
             <button type="cancel" class="btn btn-danger btn-sm">Cancel</button>
         </form>
